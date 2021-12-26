10a3f4583cec8b87b4d4a6ded3d2373d599ab23a
HeezQ <u11_wikpaw_waw@technischools.com>
2021-12-26 19:56:54 +0100

    testing features

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -9,7 +9,7 @@ module.exports =  {
         const user = await Users.findOne({ userId: message.member.id });
         if (user.messageId == message.id) return await Users.updateOne({ userId: user.userId }, { $set: { messageId: "" } })
         else if (user.randomMessageId == message.id) return await Users.updateOne({ userId: user.userId }, { $set: { randomMessageId: "" } })
-    } 
+    }
 }
 
 
